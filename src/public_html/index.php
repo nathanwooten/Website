1108501fc29fc2bc8f439e8faf21c43428ae8627
Nathan Wooten <a8gid24akL@gmail.com>
2022-09-29 02:09:23 -0400

    Update index.php

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -3,3 +3,8 @@
 $container = require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'require.php';
 
 // App...
+
+var_dump( $container );
+
+$uri = $container->get( nathanwooten\Uri\Uri::class );
+var_dump( $uri );
