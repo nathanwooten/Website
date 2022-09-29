c5c698a7c86651b68e11e6774cf39a4deba0e8c8
Nathan Wooten <a8gid24akL@gmail.com>
2022-09-29 17:31:13 -0400

    only need standard to load everything else

--- src/lib/nathanwooten/website/autoloads.php
+++ src/lib/nathanwooten/website/autoloads.php
@@ -2,29 +2,6 @@
 
 if ( ! defined( 'PROJECT_PATH' ) ) die( __FILE__ );
 
-if ( ! defined( 'PROJECT_DEPENDENCIES' ) ) {
-  $dependencies = [];
-
-  $project_path = LIB_PATH . PROJECT_NAME . DS;
-
-  $scan = scandir( $project_path );
-
-  foreach( $scan as $dir ) {
-    if ( ! is_dir( $project_path . $dir ) ) {
-      continue;
-    }
-	$directory = $dir . DS . 'src';
-
-    if ( is_dir( $project_path . $directory ) ) {
-      $namespace = PROJECT_NAME . '\\' . ucfirst( $dir );
-
-      $dependencies[] = [ $namespace, $directory ];
-    }
-  }
-
-  define( 'PROJECT_DEPENDENCIES', $dependencies );
-}
-
 if ( ! defined( 'DEPENDENCIES' ) ) define( 'DEPENDENCIES', [
 
   LIB_PATH => [
@@ -33,12 +10,6 @@ if ( ! defined( 'DEPENDENCIES' ) ) define( 'DEPENDENCIES', [
       'nathanwooten\Standard',
       'nathanwooten' . DS . 'standard' . DS . 'src'
     ],
-
-  ],
-
-  LIB_PATH . PROJECT_NAME => [
-
-    ...PROJECT_DEPENDENCIES
 
   ]
 
