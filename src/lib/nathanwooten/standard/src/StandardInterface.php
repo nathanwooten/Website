ae6d6a7550152313feca65271dffcf0a680b41d7
Nathan Wooten <a8gid24akL@gmail.com>
2022-09-29 18:24:28 -0400

    Add files via upload

---
+++ src/lib/nathanwooten/standard/src/StandardInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace nathanwooten\Standard;
+
+if ( ! interface_exists( 'nathanwooten\Standard\StandardInterface' ) ) {
+interface StandardInterface
+{
+
+  public function loadnathanwooten();
+
+}
+}
