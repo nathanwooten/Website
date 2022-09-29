d6468604fbc4ea2fb3141975da872401245b11ee
Nathan Wooten <a8gid24akL@gmail.com>
2022-09-29 17:40:09 -0400

    Update autoloads.php

--- src/lib/nathanwooten/website/autoloads.php
+++ src/lib/nathanwooten/website/autoloads.php
@@ -1,6 +1,11 @@
 <?php
 
-if ( ! defined( 'PROJECT_PATH' ) ) die( __FILE__ );
+// LIB_PATH should be defined in the /public_html/*/require.php file
+
+if ( ! defined( 'LIB_PATH' ) ) die( __FILE__ );
+
+// Standard is the class that autoloads all the standard dependencies
+// especially container, which is the dependency manager
 
 if ( ! defined( 'DEPENDENCIES' ) ) define( 'DEPENDENCIES', [
 
@@ -15,8 +20,12 @@ if ( ! defined( 'DEPENDENCIES' ) ) define( 'DEPENDENCIES', [
 
 ] );
 
+// Set the autoloads variable if not already set
+
 if ( ! isset( $autoloads ) ) {
   $autoloads = DEPENDENCIES;
 }
+
+// Return the autoloads (autoload method) autoloads
 
 return $autoloads;
