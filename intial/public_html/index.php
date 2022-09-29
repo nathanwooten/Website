6c4eb57d32e4c1849f28428bd837a8d279f36b32
Nathan Wooten <a8gid24akL@gmail.com>
2022-09-28 23:32:14 -0400

    Create index.php

---
+++ intial/public_html/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use websiteproject\{
+
+  Container\Container
+
+};
+
+use nathanwooten\{
+
+  Uri\Uri
+
+};
+
+$container = require_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'require.php';
+
+// App...
